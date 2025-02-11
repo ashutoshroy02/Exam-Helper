372f6df53e51b2820d64d80adda4ea4b8981133e
SedGram <shauryajnvkkr@gmail.com>
2025-02-11 14:21:25 +0530

    Revert Original

--- main.py
+++ main.py
@@ -361,15 +361,3 @@ if groq_api_key:
 
 
 
-
-st.components.v1.html("""
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-1923445169044240"
-     data-ad-slot="1234567890"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-  (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-""", height=300)
